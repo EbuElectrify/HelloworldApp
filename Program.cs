0d37cef5b4392adadc49bbd38aef463e2eedf4e0
EbuElectrify <EbuElectrify@gmail.com>
2022-04-02 21:41:58 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloworldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! My name is Onuh Prince Ebubechukwu, I am in Backend class of Wellahealth schools");
+            Console.WriteLine("My reason for learning Backend Development is that I'm interested in the structures and funtionalities of the web Applications and its overall conectivities.");
+        }
+    }
+}
